408bd62cb5965d95fce0916d0988dae1f3d026be
ericsonsilva <31900194+ericsonsilva@users.noreply.github.com>
2019-06-29 18:52:15 -0300

    funçóes concluídas.

--- main.lua
+++ main.lua
@@ -6,13 +6,24 @@ function love.load()
   button.size=50
 
   score=0
-  timer=0
+  timer=10
+  gameState = 1
 
   myfont = love.graphics.newFont(40)
 end
 
 function love.update(dt)
   --number = number + 1
+  if gameState == 2 then
+    if timer > 0 then
+      timer = timer - dt
+    end
+
+    if timer < 0 then
+      timer = 0
+      gameState = 1
+    end
+  end
 end
 
 function love.draw()
@@ -20,20 +31,30 @@ function love.draw()
   --love.graphics.print(number)
   love.graphics.setColor(0, 0, 1, alpha)
   love.graphics.rectangle("fill", 200, 400, 200, 100)]]
-
-  love.graphics.setColor(1, 0, 0, 1)
-  love.graphics.circle("fill", button.x, button.y, button.size)
+  if gameState == 2 then
+    love.graphics.setColor(1, 0, 0, 1)
+    love.graphics.circle("fill", button.x, button.y, button.size)
+  end
   --set score and print
   love.graphics.setFont(myfont)
   love.graphics.setColor(1, 1, 1, 1)
   love.graphics.print(score)
+  love.graphics.print(math.ceil(timer), 100, 0)
 end
 --Detecção de clique do mouse + posicionamento.
 function love.mousepressed(x, y, b, isTouch)
-  if b == 1 then
+  if b == 1 and gameState == 2 then
     if distanceBetween(button.x, button.y, love.mouse.getX(), love.mouse.getY()) < button.size then
       score = score + 1
+      button.x= math.random(button.size, love.graphics.getWidth() - button.size)
+      button.y= math.random(button.size, love.graphics.getHeight() - button.size)
     end
+  end
+
+  if gameState == 1 then
+    gameState = 2
+    timer = 10
+    score = 0
   end
 end
 -- Fórmula de cálculo de ditancia entre 2 objetos.
