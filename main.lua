019112af3013c739e516501fc150fd134bedd42a
ericsonsilva <31900194+ericsonsilva@users.noreply.github.com>
2019-06-29 17:39:43 -0300

    set button and score

--- main.lua
+++ main.lua
@@ -1,5 +1,14 @@
 function love.load()
   --number = 0
+  button={}
+  button.x=200
+  button.y=200
+  button.size=50
+
+  score=0
+  timer=0
+
+  myfont = love.graphics.newFont(40)
 end
 
 function love.update(dt)
@@ -7,7 +16,14 @@ function love.update(dt)
 end
 
 function love.draw()
-  --love.graphics.setFont(love.graphics.setNewFont(50))
+  --[[love.graphics.setFont(love.graphics.setNewFont(50))
   --love.graphics.print(number)
-  love.graphics.rectangle("fill", 200, 400, 200, 100)
+  love.graphics.setColor(0, 0, 1, alpha)
+  love.graphics.rectangle("fill", 200, 400, 200, 100)]]
+
+  love.graphics.setColor(1, 0, 0, 1)
+  love.graphics.circle("fill", button.x, button.y, button.size)
+  --set score and print
+  love.graphics.setColor(1, 1, 1, 1)
+  love.graphics.print(score)
 end
